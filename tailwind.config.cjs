0557094a0c201b5fce84eb2704015fa5f6f06695
German Fernandez <germanfernandez0306@gmail.com>
2023-02-25 16:40:50 -0300

    feat: add new color and hover only when is ready

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,svelte}'],
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   theme: {
     extend: {
       colors: {
         primary: '#3F4145',
         secondary: '#5E5E5E',
-        tertiary: '#878787'
+        tertiary: '#878787',
+        cblue: 'rgb(34, 104, 235)'
       }
     }
   },
